a48d5e1bd6baf1ce713394226d7d9523c42f915c
gdpnei2002 <joseneilson25@gmail.com>
2023-05-01 00:15:55 -0300

    estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main className="container">
       <form action="">
-        <input type="text" placeholder='' />
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='Email' />
+        <input type="tel" placeholder='telefone' />
+        <textarea placeholder='Observações'></textarea>
+        <button>Salvar</button>
       </form>
+      <div className="caixacontatos">
+        <input type="text" placeholder='buscar' />
+          <div className="caixaindividual">
+            <div className="boxtitulo">
+              <p className="nometitulo">Carla Gomes Farias</p>
+                <div>
+                  <a href="">editar</a>
+                  <a href="">excluir</a>
+                </div>
+            </div>
+            <div className="dados">
+              <p>carla@gmail</p>
+              <p>00000</p>
+              <p>amiga</p>
+            </div>
+          </div>
+      </div>
     </main>
   )
 }
